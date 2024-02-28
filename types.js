39531b204443f3cbd4456899a97253812a6ed16d
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-02-28 20:41:59 +0600

    strongly typed and dynamic typed language

---
+++ types.js
@@ -0,0 +1,20 @@
+/**
+ * ------- strongly typed language -------
+ * int a = 5;
+ * string b = 'alim halim khalim';
+ * bool c = True;
+ * object student = {name: 'noya daman' id: 55}
+ * 
+ * ==> must have to say the type of value when we declare the variable.
+ * 
+ * ==> c++, java, c, etc.
+ */
+
+
+/**
+ * --------- dynamic typed language ---------
+ * 
+ * ==> no need to mention the value type when we declare the variable.
+ * 
+ * ==> python, javaScript, etc.
+ */
